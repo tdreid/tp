e58aada0032997094e10d197edffe553aaadd430
tdreid <trevor.d.reid@gmail.com>
2018-07-14 17:16:51 +0000

    Declare CLI arguments and action

--- tp.js
+++ tp.js
@@ -7,6 +7,10 @@ const version = require('./package.json').version;
 
 tp
     .version(version)
+    .arguments('<payee> <amount> <memo> <interval>')
+    .action((payee, amount, memo, interval) => {
+        console.log('Called tp with these arguments:', payee, amount, memo, interval);  
+    })
     .parse(process.argv);
 
 if (tp.args.length === 0) tp.help();
